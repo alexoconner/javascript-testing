fa5ab8bc16215eaf2d20657c447dd2fec0cd6368
Alexander Skrabl <alexander@skrabl.de>
2016-11-20 17:51:04 +0000

    testing my react-slide-me npm module

--- reactApp.js
+++ reactApp.js
@@ -4,6 +4,19 @@ import ReactDOM from 'react-dom';
 
 import ReactTest from './reactTest';
 
+// test react-slide-me
+import MySlider from 'react-slide-me';
+
+var items = [
+    'http://lorempixel.com/500/320/',
+    'http://lorempixel.com/500/320/'
+];
+
+var size = {
+    width: 500,
+    height: 320
+}
+
 class App extends React.Component {
     constructor( props ) {
         super( props );
@@ -11,7 +24,17 @@ class App extends React.Component {
 
     render() {
         return (
-            <ReactTest />
+            <div>
+                <ReactTest />
+                <MySlider
+                    items={ items }
+                    type="image"
+                    size={ size }
+                    animation="moveHorizontal"
+                    speed={ 1000 }
+                    easing="ease"
+                />
+            </div>
         )
     }
 }
